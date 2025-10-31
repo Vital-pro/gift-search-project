d238b0db76f95e7748bd961d68a7dcf6026eddd4
Vital-pro <vitupikov@yandex.ru>
2025-10-31 17:31:35 +0300

    relocate the images folder

--- gift-search-site/data/gifts/categoryPlaceholderImages.js
+++ gift-search-site/data/gifts/categoryPlaceholderImages.js
@@ -1,35 +1,28 @@
-// Этот файл содержит URL-ы изображений-заглушек для различных категорий.
-// Используется, когда у конкретного подарка нет собственного изображения.
-
-// 'https://ad.admitad.com/dummy/?w=2872025&c=34787&r=3&d=3&u=https%3A%2F%2Fboobl-goom.ru%2Fproduct%2Figra-hobby-worl-nastolnaya-voobrazhariy-na-eruditsiyu-i-nakhodchivost%2F&g=RU&x=eaa28a92b5376b3906bc16f74d5fb9da&y=31a3fcaa11cd0097347387ba8ee20c4c';
-
-// 'https://offerwall.admitad.com/wall/offers/?w=2872025&c=34787&r=3&d=3&u=https%3A%2F%2Fboobl-goom.ru%2Fproduct%2Figra-hobby-worl-nastolnaya-voobrazhariy-na-eruditsiyu-i-nakhodchivost%2F&g=RU&x=eaa28a92b5376b3906bc16f74d5fb9da&y=31a3fcaa11cd0097347387ba8ee20c4c'; //! 404 from Admitad??
-
 export const categoryPlaceholderImages = {
-  beauty: 'assets/images/category_images/beauty/beauty-woman-makeup.jpg',
-  health: 'assets/images/category_images/flowers/flowers.jpg',
-  tech: 'assets/images/category_images/tech/tech.jpg',
-  hobby: 'assets/images/category_images/creative/creative.jpg',
-  tools: 'assets/images/category_images/creative/сreative-1.jpg',
-  toys: 'assets/images/category_images/toys/toys-new.jpg',
-  education: 'assets/images/category_images/creative/hobby-1.jpg',
-  creative: 'assets/images/category_images/creative/сreative-1.jpg',
-  jewelry: 'assets/images/category_images/flowers/flowers-2.jpg',
-  perfume: 'assets/images/category_images/flowers/flowers-9.jpg',
-  sport: 'assets/images/category_images/adventures.jpg',
-  office: 'assets/images/category_images/universal/universal-1.jpg',
-  food: 'assets/images/category_images/universal/universal-1.jpg',
-  home: 'assets/images/category_images/flowers/flowers-3.jpg',
+  beauty: '/images/category_images/beauty/beauty-woman-makeup.jpg',
+  health: '/images/category_images/flowers/flowers.jpg',
+  tech: '/images/category_images/tech/tech.jpg',
+  hobby: '/images/category_images/creative/creative.jpg',
+  tools: '/images/category_images/creative/сreative-1.jpg',
+  toys: '/images/category_images/toys/toys-new.jpg',
+  education: '/images/category_images/creative/hobby-1.jpg',
+  creative: '/images/category_images/creative/сreative-1.jpg',
+  jewelry: '/images/category_images/flowers/flowers-2.jpg',
+  perfume: '/images/category_images/flowers/flowers-9.jpg',
+  sport: '/images/category_images/adventures.jpg',
+  office: '/images/category_images/universal/universal-1.jpg',
+  food: '/images/category_images/universal/universal-1.jpg',
+  home: '/images/category_images/flowers/flowers-3.jpg',
   photo:
-    'assets/images/category_images/creative/сreative-1.jpg',
+    '/images/category_images/creative/сreative-1.jpg',
   entertainment:
-    'assets/images/category_images/adventures.jpg',
+    '/images/category_images/adventures.jpg',
   books:
-    'assets/images/category_images/toys/toys-retro.jpg',
+    '/images/category_images/toys/toys-retro.jpg',
   outdoor:
-    'assets/images/category_images/adventures.jpg',
+    '/images/category_images/adventures.jpg',
   universal:
-    'assets/images/category_images/universal/universal.jpg',
+    '/images/category_images/universal/universal.jpg',
   boardgames:
-    'assets/images/category_images/toys/toys-boardgames.jpg',
+    '/images/category_images/toys/toys-boardgames.jpg',
 };
